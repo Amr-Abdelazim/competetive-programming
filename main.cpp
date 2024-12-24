e9ab41d96513aa5923cacbe84005ea0af6c2b246
Amr Abdelazim <amrabdelazim100@gmail.com>
2024-12-24 11:33:03 +0200

    ternary search and debug added!

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
-#ifdef ONLINE_JUDGE
+#ifndef LOCAL
 #include <bits/stdc++.h>
 // For ordered_set
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
+#define debug(...) 8
 #else
 #include "precompiled.h"
+#include "debug.h"
 #endif
 
 using namespace std;
@@ -14,10 +16,11 @@ using ld = long double;
 using uint = unsigned int;
 using ull = unsigned long long;
 using pii = pair<int, int>;
-using pli = pair<ll, int>;
 using pll = pair<ll, ll>;
 #define all(v) v.begin(), v.end()
 #define rall(v) v.rbegin(), v.rend()
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define bit(mask, i) (((mask) >> (i)) & 1)
 
 template <typename T>
 using ord_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
